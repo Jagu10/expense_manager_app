f83adf4370d1d9a4f78b35994fecce5125b76abb
jagu10 <jagugojiya10@gmail.com>
2026-03-18 19:45:30 +0530

    done

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,14 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "./generated/prisma/client";
- 
-const adapter = new PrismaMariaDb({ 
-    host:"localhost", 
-    port:3306, 
-    user:"root", 
-    password:"18E200@e", 
-    database:"expense_manager", 
-    connectionLimit:5 
-}) 
- 
-export const prisma = new PrismaClient({adapter}); 
+import { PrismaClient } from './generated/prisma/client';
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_4eP7qIQoOGXw@ep-raspy-tree-amfebhob-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; 
--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { url } from "inspector";
-import { defineConfig } from "prisma/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -10,6 +10,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["DATABASE_URL"]!,
   },
 });
